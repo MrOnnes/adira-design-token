97c829eb4b24e078993886a5d93948fa6c794e1a
senno adi <79636269+undefined@users.noreply.github.com>
2022-11-04 10:01:50 +0000

    Push from Documentation

--- size/stories/css-showcases.stories.js
+++
@@ -1,9 +0,0 @@
-export const border_radius = () => /*html*/ `<dockit-css-showcases
-  css-props-prefix="--figma-size-radius"
-  component-class="box wide"
-  style-key="border-radius"
-></dockit-css-showcases>`;
-
-export const spacing = () => /*html*/ `<dockit-css-showcases
-  css-props-prefix="--figma"
-></dockit-css-showcases>`;
