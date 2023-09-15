c526952e086e27e36212f0b58450bc33d96613a3
senno adi <senno.adi2509@gmail.com>
2023-09-15 15:37:58 +0000

    chore: style-dictionary output

--- tokens/dart/dart_color.dart
+++ tokens/dart/dart_color.dart
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 15 Sep 2023 15:37:00 GMT
+// Generated on Fri, 15 Sep 2023 15:37:57 GMT
 
 
 
@@ -64,7 +64,7 @@ class adrColor {
     static const textNormal = Color(0xFF344054);
     static const textSidebar = Color(0xFF344054);
     static const textSuccess = Color(0xFF12B76A);
-    static const textTest = Color(0xFF522FEE);
+    static const textTest = Color(0xFF522FBB);
     static const textWarning = Color(0xFFF79009);
     static const textWhite = Color(0xFFFCFCFD);
 }
