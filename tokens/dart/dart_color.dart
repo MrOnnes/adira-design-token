7cb3c7d32c9ea5cb72a04d22b99f9b00ad38d183
Senno Adi (Robot style dictionary converter) <example@gmail.com>
2023-09-15 04:02:04 +0000

    chore: style-dictionary output

--- tokens/dart/dart_color.dart
+++ tokens/dart/dart_color.dart
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 15 Sep 2023 03:58:09 GMT
+// Generated on Fri, 15 Sep 2023 04:02:03 GMT
 
 
 
@@ -64,7 +64,7 @@ class adrColor {
     static const textNormal = Color(0xFF344054);
     static const textSidebar = Color(0xFF344054);
     static const textSuccess = Color(0xFF12B76A);
-    static const textTest = Color(0xFF522FBB);
+    static const textTest = Color(0xFF522FCC);
     static const textWarning = Color(0xFFF79009);
     static const textWhite = Color(0xFFFCFCFD);
 }
